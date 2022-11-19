c67399b5a53a7d6d7b103ae9ac72d0b2c058181c
Ibrahim Turan <hello@ibrahimturan.com>
2022-11-19 22:12:41 +0300

    feat: add i18n vie config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,23 @@
-import { defineConfig } from 'vite'
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueI18n from '@intlify/vite-plugin-vue-i18n';
 import path from 'path';
-import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    vueI18n({
+      runtimeOnly: true,
+      compositionOnly: true,
+      include: [path.resolve(__dirname, 'src/locales/**')],
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
       '@ui': path.resolve(__dirname, './src/components/ui'),
     },
   },
-})
+});
