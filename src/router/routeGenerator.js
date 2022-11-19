86d1a4fcaab6365145fdd5319728307c07269f67
Ibrahim Turan <hello@ibrahimturan.com>
2022-11-19 23:20:54 +0300

    refactor: improve route generator func performance

--- src/router/routeGenerator.js
+++ src/router/routeGenerator.js
@@ -1,27 +1,25 @@
 import routes from '@/router/routes';
 import { localesWithoutEn } from '@/locales';
 
+const generateRoute = (value, lang, isChild = false) => {
+  const newRoute = {
+    ...value,
+    ...(!isChild && { path: `${lang ? '/' : ''}${lang}${value.path[lang || 'en']}` }),
+    ...(isChild && { path: value.path[lang || 'en'] }),
+    ...(value.redirect && { redirect: { name: `${lang || 'en'}-${value.redirect.name}` } }),
+    name: `${lang || 'en'}-${value.name}`,
+  };
+
+  if (value.children && value.children.length) {
+    newRoute.children = value.children
+      .map((item) => generateRoute(item, true));
+  }
+
+  return newRoute;
+};
+
 export default localesWithoutEn.flatMap((lang) => (
   [
-    ...routes.map((route) => {
-      const generateRoute = (value, isChild = false) => {
-        const newRoute = {
-          ...value,
-          ...(!isChild && { path: `${lang ? '/' : ''}${lang}${value.path[lang || 'en']}` }),
-          ...(isChild && { path: value.path[lang || 'en'] }),
-          ...(value.redirect && { redirect: { name: `${lang || 'en'}-${value.redirect.name}` } }),
-          name: `${lang || 'en'}-${value.name}`,
-        };
-
-        if (value.children && value.children.length) {
-          newRoute.children = value.children
-            .map((item) => generateRoute(item, true));
-        }
-
-        return newRoute;
-      };
-
-      return generateRoute(route);
-    }),
+    ...routes.map((route) => generateRoute(route, lang)),
   ]
 ));
